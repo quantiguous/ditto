3ca39238433975ff10ff01bcb345230231aeda19
NileshPanchal <neil08.panchal@gmail.com>
2016-01-04 17:04:25 +0530

    logs screen

--- app/controllers/request_logs_controller.rb
+++ app/controllers/request_logs_controller.rb
@@ -2,21 +2,27 @@ class RequestLogsController < ApplicationController
 
   def index
     if !params[:route_id].present?
-      @request_logs = RequestLog.all
+      request_logs = RequestLog.order("id desc")
     else
       @route = Route.find(params[:route_id]) rescue nil
       if !@route.nil?
-        @request_logs = @route.request_logs
+        request_logs = @route.request_logs
       else
         flash[:notice] = "Route with id #{params[:route_id]} doesn't exists."
         redirect_to "/"
       end
     end
+    @request_logs_count = request_logs.count
+    @request_logs = request_logs.paginate(:per_page => 10, :page => params[:page]) rescue []
   end
 
   def show
-    require 'yaml'
     @request_log = RequestLog.find(params[:id])
+    @req_resp_details = {}
+    @request_headers =  @request_log.headers
+    if !@request_log.headers.nil?
+      @req_resp_details = {request_details: @request_log.headers.first, response_details: @request_log.headers.last}
+    end
   end
 
 end
